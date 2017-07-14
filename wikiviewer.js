39e562222e31409ee80a9e556995e59c335e7fa2
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-14 19:06:01 -0400

    Added a json method that calls the wikipedia API and added jquery that will populate that information into a table

--- wikiviewer.js
+++ wikiviewer.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
     var entry_row = $(".entry");
 
     submit_button.click(function() {
-        alert("button clicked"); 
+
     });
 
+    // Change color of side bars when user hovers mouse
+
     entry_row.mouseenter(function() {
-        //console.log(this.children().eq(1));
         $(this).children().eq(0).css("background-color","lightblue");
         $(this).children().eq(2).css("background-color","lightblue");
     });
@@ -17,4 +18,28 @@ $(document).ready(function() {
         $(this).children().eq(0).css("background-color","white");
         $(this).children().eq(2).css("background-color","white");
     });
+
+    //change content of entries using wiki api
+
+    var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=bob&format=json&callback=?";
+
+
+    $.getJSON(api, function(data) {
+        var prefixH = "#sh";
+        var prefixP = "#sp";
+        var prefixA = "#a";
+        var count = 1;
+        for(var i = 1; i <= 7; i++) {
+            var curr_h = prefixH + i;
+            var curr_p = prefixP + i;
+            var curr_a = prefixA + i;
+            console.log(data[1][count])
+            $(curr_h).html(data[1][count]);
+            $(curr_p).html(data[2][count]);
+            $(curr_a).attr('href', data[3][count]);
+            count++;
+        }
+    });
+
+
 });
