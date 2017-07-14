26ee8763a85137a32aa9dd3838dd7d868ceb1597
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-14 19:52:32 -0400

    Fully implemented the javascript to display results and implmeneted an image to be displayed when no results are

--- wikiviewer.js
+++ wikiviewer.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
     var submit_button = $("#search");
     var input_field = $("#input");
-    var input_text = "";
     var entry_row = $(".entry");
+    var bottom = $(".bottom");
+    var pic = $("#pic");
+    var random = $("#random");
+
+    // Take user input and call methods to display to screen
 
     submit_button.click(function() {
-
+        pic.css("display", "none");
+        bottom.css("display", "inherit");
+        var searchQuery= input_field.val();
+        if(searchQuery==="") {
+            pic.css("display", "inherit");
+            bottom.css("display", "none");
+        } else {
+            displayResults(searchQuery);
+        }
     });
+
 
     // Change color of side bars when user hovers mouse
 
@@ -21,25 +34,27 @@ $(document).ready(function() {
 
     //change content of entries using wiki api
 
-    var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=bob&format=json&callback=?";
+    function displayResults(searchQuery) {
+        var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchQuery+"&format=json&callback=?";
 
+        $.getJSON(api, function(data) {
+            var prefixH = "#sh";
+            var prefixP = "#sp";
+            var prefixA = "#a";
+            var count = 1;
+            var flag = false;
+            for(var i = 1; i <= 7; i++) {
+                var curr_h = prefixH + i;
+                var curr_p = prefixP + i;
+                var curr_a = prefixA + i;
+                $(curr_h).html(data[1][count]);
+                $(curr_p).html(data[2][count]);
+                $(curr_a).attr('href', data[3][count]);
+                count++;
+            }
+        });
+    }
 
-    $.getJSON(api, function(data) {
-        var prefixH = "#sh";
-        var prefixP = "#sp";
-        var prefixA = "#a";
-        var count = 1;
-        for(var i = 1; i <= 7; i++) {
-            var curr_h = prefixH + i;
-            var curr_p = prefixP + i;
-            var curr_a = prefixA + i;
-            console.log(data[1][count])
-            $(curr_h).html(data[1][count]);
-            $(curr_p).html(data[2][count]);
-            $(curr_a).attr('href', data[3][count]);
-            count++;
-        }
-    });
 
 
 });
